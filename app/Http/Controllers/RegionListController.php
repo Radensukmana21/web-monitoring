2ec07dd9b15a1764db1a1484a2daf101760ab9c3
Hanif Muthiar Tsani <hanifmuthiartsani791@gmail.com>
2025-05-19 21:40:00 +0700

    regionlistcontroller

--- app/Http/Controllers/RegionListController.php
+++ app/Http/Controllers/RegionListController.php
@@ -30,36 +30,3 @@ class RegionListController extends Controller
     }
 
 }
-
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\Region;
-use App\Models\IncomingFile;
-
-class RegionListController extends Controller
-{
-    public function index(Request $request)
-    {
-        $regions = Region::with(['partners' => function ($query) {
-            $query->withCount(['incomingFiles as file_count' => function ($q) {
-                $q->leftJoin('archived_files', function ($join) {
-                    $join->on('incoming_files.filename', '=', 'archived_files.filename')
-                         ->on('incoming_files.partner_id', '=', 'archived_files.partner_id');
-                })
-                ->whereNull('archived_files.id');
-            }]);
-        }])->withCount(['incomingFiles as file_count' => function ($q) {
-            $q->leftJoin('archived_files', function ($join) {
-                $join->on('incoming_files.filename', '=', 'archived_files.filename')
-                     ->on('incoming_files.partner_id', '=', 'archived_files.partner_id');
-            })
-            ->whereNull('archived_files.id');
-        }])->paginate(5);
-
-        return view('regions.index', compact('regions'));
-    }
-}
